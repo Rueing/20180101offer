70aecda2ddd61c0b33414503576b43c1c47e54b5
Rueing <583710220@qq.com>
2018-01-15 19:20:04 +0800

    Create 19二叉树的镜像.cpp

---
+++ 19二叉树的镜像.cpp
@@ -0,0 +1,29 @@
+//19
+// 思路：
+// 递归
+// 遍历每个结点，交换其左右子树。(左右都为叶结点也没关系)
+
+/*
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};*/
+class Solution {
+public:
+    void Mirror(TreeNode *pRoot)
+    {
+    	if (pRoot == nullptr)
+    		return;
+		  //前序
+    	tempNode = pRoot->left;
+    	pRoot->left = pRoot->right;
+    	pRoot->right = tempNode;
+
+    	Mirror(pRoot->left);
+    	Mirror(pRoot->right);
+    }
+};
