65fcd8e4b4e05fc28bd9dc54331cd51e859c90bc
Rueing <583710220@qq.com>
2018-01-15 10:26:50 +0800

    Create 10二进制中1的个数.cpp

---
+++ 10二进制中1的个数.cpp
@@ -0,0 +1,14 @@
+//10
+//减一与
+class Solution {
+public:
+     int  NumberOf1(int n) {
+         int count = 0;
+         while(n)
+         {
+             ++count;
+             n = (n-1) & n;
+         }
+         return count;
+     }
+};
