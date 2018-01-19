e8f8e04de11f36646c331276ac30f3a606225f46
Rueing <583710220@qq.com>
2018-01-19 15:22:29 +0800

    Create 40数组中只出现一次的数字.cpp

---
+++ 40数组中只出现一次的数字.cpp
@@ -0,0 +1,35 @@
+//40
+// 一个整型数组里除了两个数字之外，其他的数字都出现了两次。请写程序找出这两个只出现一次的数字。
+
+// 思路；先考虑这么一个问题：一个整型数组里除了1个数字只出现1次之外，其余都出现了2次。
+// 		 进行位运算：与，即可得到这个数字	 
+// 思维迁移：把这2个只出现1次的数字分离开来。
+// 			 做“与”之后的二进制表示中必有一位不为0，以此来划分为2个子数组。
+// 位运算时，注意优先级，保险加上括号！
+class Solution {
+public:
+    void FindNumsAppearOnce(vector<int> data, int* num1, int *num2) 
+    {
+    	if (data.empty())
+    		return;
+    	unsigned int xor_final = 0;
+    	for (int i = 0; i < data.size(); ++i)
+    		xor_final ^= data[i];
+
+    	//寻找这2个数，在二进制表示中其中一个不同的地方
+    	int AUnlikeflag = 0;
+    	while (((xor_final & 1) == 0) && (AUnlikeflag < 8 * sizeof(int))) //AUnlikeflag < 8 * sizeof(int) 可省略?
+    	{
+    		++AUnlikeflag;
+    		xor_final = xor_final >> 1;
+    	}
+
+    	for (int j = 0; j < data.size(); ++j)
+    	{
+    		if (((data[j] >> AUnlikeflag) & 1) == 0)
+    			*num1 ^= data[j];
+    		else
+    			*num2 ^= data[j];
+    	}
+    }
+};
