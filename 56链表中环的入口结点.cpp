a43afe252836ff290b2357e099d24eace6d60163
Rueing <583710220@qq.com>
2018-01-24 11:13:47 +0800

    Update 56链表中环的入口结点.cpp

--- 56链表中环的入口结点.cpp
+++ 56链表中环的入口结点.cpp
@@ -11,9 +11,9 @@ struct ListNode {
 };
 
 //思路：快慢指针(2倍速度)
-//		 相遇时2x=x+nr --> x=nr（可取n=1，没什么影响）
-//		 x=r，于是画图可知：让两指针分别从头结点、相遇结点出发
-//		 相遇结点即为环的入口结点
+//	相遇时2x=x+nr --> x=nr（可取n=1，没什么影响）
+//	x=r，于是画图可知：让两指针分别从头结点、相遇结点出发
+//	相遇结点即为环的入口结点
 
 //注意：若链表不具有环，那么存在->next=nullptr;
 */
@@ -40,4 +40,39 @@ public:
     	}
     	return nullptr;
     }
+
+    //法2：修改了链表
+    ListNode* EntryNodeOfLoop2(ListNode* pHead)
+    {
+        if (pHead == NULL || pHead->next == NULL)
+            return NULL;
+
+        ListNode* previous = pHead;
+        ListNode* current = pHead->next;
+        while (current != NULL)
+        {
+            previous->next = NULL;
+            previous = current;
+            current = current->next;
+        }
+        return previous;
+    }
+
+    //法3：哈希表
+	// 用到了STL中的set，set有一个特性就是不能插入相同元素，
+	// 这样只需遍历原List一次就可以判断出有没有环，还有环的入口地址。
+	// s.insert(node).second这里在插入的同时也判断了插入是否成功，
+	// 如果不成功表明set中已经有该元素了，该元素就是环的入口元素。
+    ListNode* EntryNodeOfLoop3(ListNode* pHead)
+    {
+        set<ListNode*> s;
+        ListNode* node = pHead;
+        while (node != NULL){
+            if (s.insert(node).second)
+                node = node->next;
+            else
+                return node;
+        }
+        return node;  
+    }
 };
